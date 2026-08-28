d554b6260ab9459e62b0ba4ece73ae5e8e8c65b7
mohitkumar576665 <mohit.kumar162534123@gmail.com>
2026-08-29 03:50:48 +0530

    Add Two Sum solution

---
+++ solutions/Hard/two-sum.cpp
@@ -0,0 +1,8 @@
+        for(int i =0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if(nums[i] + nums[j]==target) return {i,j};
+            }
+        // return {};
+        int n = nums.size();
+    vector<int> twoSum(vector<int>& nums, int target) {
+public:
